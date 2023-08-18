a6c3cd347943cb661abedc8167693cf32f3ee2b1
JulianOcampo <juliancamilocampo@gmail.com>
2023-08-18 18:55:31 -0500

    añadir configuracion de swagger

--- src/main.azure.ts
+++ src/main.azure.ts
@@ -1,11 +1,19 @@
 import { INestApplication } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 export async function createApp(): Promise<INestApplication> {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  
+
+  const builder = new DocumentBuilder()
+    .setTitle('Mi Casa')
+    .setDescription('Mi Casa')
+    .build();
+  const document = SwaggerModule.createDocument(app, builder);
+  SwaggerModule.setup('api', app, document);
+
   await app.init();
   return app;
 }
