ebe1036b0168b91c56e1252fa9e93eff605de06a
JulianOcampo <juliancamilocampo@gmail.com>
2023-08-18 19:52:29 -0500

    adjust host

--- src/main.azure.ts
+++ src/main.azure.ts
@@ -2,10 +2,14 @@ import { INestApplication } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
 export async function createApp(): Promise<INestApplication> {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.setGlobalPrefix('api');
+  const config = app.get(ConfigService);
+  const port = config.get('port');
 
   const builder = new DocumentBuilder()
     .setTitle('Mi Casa')
@@ -15,5 +19,7 @@ export async function createApp(): Promise<INestApplication> {
   SwaggerModule.setup('api', app, document);
 
   await app.init();
+  await app.listen(port);
+
   return app;
 }
